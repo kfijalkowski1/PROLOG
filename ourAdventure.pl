104e7cf1c53a87dadd49cbef0802353bc6d0501e
jwozniak <01169260@pw.edu.pl>
2023-11-10 15:34:53 +0100

    opisy na wejście

--- ourAdventure.pl
+++ ourAdventure.pl
@@ -132,35 +132,141 @@ path(tundra, n, jaskinia1).
 path(tundra, e, stepy).
 
 % description
-describe(pustkowie2) :- write(''), nl.
-describe(oceanStatek) :- write(''), nl.
-describe(oceanPusty) :- write(''), nl.
-describe(rafaKoralowa) :- write(''), nl.
-describe(jaskiniaPodwodna) :- write(''), nl.
+describe(pustkowie2) :-
+    write('Wędrujesz przez pustkowie, gdzie krajobraz jest surowy, a ziemia spękana od przesuszenia.'), nl,
+    write('Nie ma tu żadnych oznak życia, a jedynym towarzyszem jest oszałamiające słońce na bezchmurnym niebie.'), nl,
+    write('W oddali dostrzegasz samotne formacje skalne, które zdają się być ostatnimi świadkami dawnej obfitości tego terenu.'), nl,
+    write('Pustkowie emanuje atmosferą opuszczenia, a cisza jest jedynym dźwiękiem, jaki się tu rozchodzi.'), nl.
 
-describe(obozRozbojnikow) :- write(''), nl.
-describe(rzeka) :- write(''), nl.
-describe(kanion) :- write(''), nl.
-describe(lasBrzozowy) :- write(''), nl.
-describe(jaskinia2) :- write(''), nl.
+describe(oceanStatek) :-
+    write('Stoisz na brzegu spokojnych wód oceanu, gdzie horyzont łączy się z niebem w jednym nieskończonym punkcie.'), nl,
+    write('W oddali dostrzegasz zatopiony wrak statku, jego zardzewiałe fragmenty unoszą się na powierzchni wody.'), nl,
+    write('Pęknięte burty i rozerwane żagle świadczą o dawnej chwale tego statku, który teraz spoczywa na dnie oceanu.'), nl,
+    write('Słyszysz odgłos fal oblewających wrak, a powietrze unosi się zapachem morskiej soli i historii opowiedzianej przez zatopiony statek.'), nl.
 
-describe(gory) :- write(''), nl.
-describe(jezioro) :- write(''), nl.
-describe(laka) :- write('jestes na pieknej lace'), nl.
-describe(lasDebowy) :- write(''), nl.
-describe(wysokieGory) :- write(''), nl.
+describe(oceanPusty) :-
+    write('Nurkujesz w głąb oceanu, a wokół ciebie rozciąga się pusty, bezkresny obszar wód głębinowych.'), nl,
+    write('Brak jakiejkolwiek roślinności czy form życia, jedynie monotonny krajobraz niekończących się wód oceanicznych.'), nl,
+    write('Ciemność otacza cię ze wszystkich stron, a jedynym dźwiękiem jest łagodne szumienie wody przemieszczanej przez ruchy fal.'), nl,
+    write('To miejsce emanuje spokojem, ale jednocześnie wzbudza uczucie nieskończoności i tajemnicy oceanicznej głębi.'), nl.
 
-describe(jaskinia1) :- write(''), nl.
-describe(tajga) :- write(''), nl.
-describe(wioska) :- write(''), nl.
-describe(dolina) :- write(''), nl.
-describe(zniszczonyPortal) :- write(''), nl.
+describe(rafaKoralowa) :-
+    write('Zanurzasz się w barwnym świecie rafy koralowej, gdzie tysiące koralowców tworzy niezwykłe formacje podwodne.'), nl,
+    write('Rafa pulsuje życiem, a kolorowe ryby przeplatają się między koralami, tworząc hipnotyzujący taniec pod wodą.'), nl,
+    write('Na dnie rafy dostrzegasz ukryte jaskinie i groty, które stanowią schronienie dla różnorodnych morskich istot.'), nl,
+    write('Dźwięki wody i śpiewu morskich istot wypełniają przestrzeń, nadając temu miejscu magii i niepowtarzalnego piękna.'), nl.
 
-describe(tundra) :- write(''), nl.
-describe(stepy) :- write(''), nl.
-describe(pustynia) :- write(''), nl.
-describe(mesa) :- write(''), nl.
-describe(pustkowie1) :- write(''), nl.
+describe(jaskiniaPodwodna) :-
+    write('Nurkujesz głęboko w czeluście podwodnej jaskini, gdzie światło słoneczne ledwo przenika przez kryształową wodę.'), nl,
+    write('Ściany jaskini są pokryte koralowymi formacjami, a kolorowe ryby migoczą wśród zakamarków, tworząc podwodne arcydzieło natury.'), nl,
+    write('Woda jest przejrzysta, a na dnie jaskini dostrzegasz ukryte jaskinie i szczeliny, które prowadzą do nieznanych zakątków podwodnego świata.'), nl,
+    write('Dźwięki wody i subtelne szmery morskich istot wypełniają przestrzeń, nadając temu miejscu magiczny i tajemniczy charakter.'), nl.
+
+describe(obozRozbojnikow) :-
+    write('Znajdujesz się przed obozem rozbójników, ukrytym głęboko w gęstym lesie, z dala od cywilizacji.'), nl,
+    write('Na obrzeżach obozu widzisz prymitywne szałasy zrobione z gałęzi i skór, a dym z ognisk unosi się w powietrze.'), nl,
+    write('Wokół obozu czujesz podejrzane spojrzenia, a z oddali dobiegają odgłosy dzikich zwierząt, które współistnieją z rozbójnikami.'), nl,
+    write('Strzeż się, gdyż to miejsce pełne jest tajemnic i niebezpieczeństw, które czyhają na każdego, kto zbliży się zbyt blisko.'), nl.
+
+describe(rzeka) :-
+    write('Stoisz na brzegu urokliwego jeziora, gdzie spokojna rzeka płynąca z gór wpada w jego nurt, tworząc malownicze połączenie wód.'), nl,
+    write('Miejsce to emanuje harmonią natury, gdzie szum rzeki miesza się z delikatnym falowaniem jeziora.'), nl,
+    write('Ptaki wodne unoszą się na powierzchni jeziora, a nad brzegiem rosną kolorowe kwiaty, zdobiąc otoczenie tego magicznego zakątka.'), nl,
+    write('Woda jeziora mieni się odcieniami niebieskiego, a promienie słońca odbijają się na powierzchni, jakby malowały błyszczący obraz natury.'), nl.
+
+describe(kanion) :-
+    write('Stoisz na krawędzi wielkiego kanionu, gdzie ściany skalne wznoszą się ku niebu, tworząc imponujący labirynt głębokich wąwozów.'), nl,
+    write('W dnie kanionu płynie rwąca rzeka, której wody błyszczą w świetle słońca, tworząc błękitną nitkę wśród skalistego krajobrazu.'), nl,
+    write('Na ścianach skalnych dostrzegasz warstwy skał, które zdają się opowiadać historię wielowiekowych procesów geologicznych.'), nl,
+    write('W powietrzu unosi się świeży zapach wilgotnej ziemi, a echo odgłosów natury tworzy magię tego niezwykłego miejsca.'), nl.
+
+describe(lasBrzozowy) :-
+    write('Przechadzasz się przez uroczy las brzozowy, gdzie delikatne liście brzóz szumią w letnim wietrze.'), nl,
+    write('Pod stopami czujesz miękki dywan z opadłych liści, a promienie słońca przebijające się przez korony drzew malują złote plamy na ziemi.'), nl,
+    write('Ptaki śpiewają miłe melodie, tworząc harmonijną symfonię natury, a ścieżki wiodą cię między korzeniami i pniami brzóz.'), nl,
+    write('W oddali słychać szmer strumienia, który dodaje uroku temu malowniczemu zakątkowi lasu.'), nl.
+
+describe(jaskinia2) :-
+    write('Wkraczasz do jaskini, a wnętrze mieni się blaskiem tysięcy kryształów o różnych kształtach i kolorach.'), nl,
+    write('Ściany jaskini są dosłownie pokryte błyszczącymi klejnotami, które odbijają światło w fantastyczny sposób.'), nl,
+    write('W powietrzu unoszą się iskry, tworząc magiczną aurę wokół ciebie, gdy kroczysz po posypanym kryształami podłożu.'), nl,
+    write('Delikatne dźwięki echo odbijają się od kryształów, tworząc hipnotyzującą symfonię, której nigdy wcześniej nie słyszałeś.'), nl.
+
+describe(gory) :-
+    write('Wspinasz się po majestatycznych szczytach gór, gdzie powietrze staje się rzadkie, a panorama roztacza się na wszystkie strony.'), nl,
+    write('Skały odbijają odcienie wschodzącego słońca, malując krajobraz w ciepłych kolorach pomarańczy i różu.'), nl,
+    write('Na szczytach gór błyszczą śnieżnobiałe czapki, a stoki porośnięte są kolorowymi kwiatami i mchami.'), nl,
+    write('W dolinach górskich widać kręte rzeki, które biegną w dół, dodając życia temu surowemu, ale pięknemu otoczeniu.'), nl.
+
+describe(jezioro) :-
+    write('Stoisz na brzegu spokojnego jeziora, którego powierzchnia migocze w blasku promieni słonecznych.'), nl,
+    write('Woda jest krystalicznie czysta, a na jej powierzchni unoszą się kolorowe liście i płyną delikatne fale.'), nl.
+
+describe(laka) :-
+    write('Jesteś na malowniczej łące, gdzie zielenią traw rozbłyskują kolorowe kwiaty.'), nl,
+    write('Ptaki śpiewają w gałęziach drzew, a w oddali słychać szmer strumyka.'), nl.
+
+describe(wysokieGory) :-
+    write('Stoisz w cieniu potężnych szczytów wysokich gór, gdzie powietrze jest rzadkie, a krajobraz zapiera dech w piersiach.'), nl,
+    write('Na ich szczytach błyszczą wieczne lodowce, a w dolinach unosi się lekka mgła.'), nl,
+    write('Słyszysz szum wiatru między skałami, a widok sięga daleko poza horyzont.'), nl.
+
+describe(jaskinia1) :-
+    write('Wchodzisz do ciemnej jaskini, gdzie ciemność otacza cię ze wszystkich stron.'), nl,
+    write('Mroczne tunele wydają się prowadzić głęboko pod ziemię.'), nl,
+    write('Słyszysz odgłosy kapryśnych kropli wody, a ściany jaskini są pokryte tajemniczymi rysunkami.'), nl.
+
+describe(tajga) :-
+    write('Przechadzasz się przez tajgę, gdzie drzewa sosnowe gęsto porastają obszar wokół ciebie.'), nl,
+    write('Pod stopami chrupie suchy mech, a powietrze nasycone jest zapachem żywicy.'), nl,
+    write('W oddali słychać szmer strumienia, a w górze przelatują kolorowe ptaki.'), nl.
+
+describe(wioska) :-
+    write('Przechadzasz się przez malowniczą wioskę, gdzie drewniane domy układają się wzdłuż brukowanych uliczek.'), nl,
+    write('Ludzie rozmawiają ze sobą na ulicach, a dzieci biegają wesoło wokół zabawek.'), nl,
+    write('Na rynku widzisz kolorowe stragany, na których sprzedawane są świeże owoce i rękodzieła.'), nl.
+
+describe(dolina) :-
+    write('Przechadzasz się po spokojnej dolinie, gdzie z obu stron wznoszą się zielone wzgórza.'), nl,
+    write('W dolinie kwitną kolorowe kwiaty, a delikatny wiatr niesie ze sobą świeży zapach traw i roślin.'), nl,
+    write('W oddali widzisz cichą rzekę, która biegnie przez środek doliny, dodając jej uroku.'), nl.
+
+describe(zniszczonyPortal) :-
+    write('Stoisz przed zniszczonym portalem, którego kształt przypomina pozostałości po dawnych potęgach magicznych.'), nl,
+    write('Jego kamienne bloki są popękane, a magiczne symbole na powierzchni zatraciły swój dawny blask.'), nl,
+    write('Wokół portalu panuje dziwna atmosfera, jakby pozostałości magii wciąż unosiły się w powietrzu.'), nl,
+    write('Głębokie rysy zdobią struktury portalu, świadcząc o jego wielowiekowej historii i potężnych wydarzeniach.'), nl,
+    write('Z dala słyszysz szepty przeszłości, które niosą opowieści o dawnych podróżach między światami.'), nl.
+
+describe(tundra) :-
+    write('Przechadzasz się po rozległej tundrze, gdzie ziemia jest pokryta warstwą mchu i niskich krzewów, a drzewa są rzadkością.'), nl,
+    write('Pod stopami czujesz chłód wilgotnej gleby, a w powietrzu unosi się delikatny zapach mchu i roślinności tundry.'), nl,
+    write('Na horyzoncie widzisz majestatyczne góry pokryte śniegiem, które kontrastują z płaskim krajobrazem tundry.'), nl,
+    write('W oddali słychać odgłosy ptaków, które przemierzają niebo, a podczas wiatru trawy tundry poruszają się jak ocean fal.'), nl.
+
+describe(stepy) :-
+    write('Przechadzasz się po rozległych stepach, gdzie trawa faluje na wietrze, tworząc ocean zielonej roślinności.'), nl,
+    write('Pod stopami czujesz miękki dywan trawy, a w oddali widzisz grupy dzikich koni swobodnie biegających po stepach.'), nl,
+    write('Niebo jest bezchmurne, a słońce rzuca złote promienie na obszar stepów, dodając im blasku.'), nl,
+    write('Stepy są ciche, ale wiatr niesie ze sobą dźwięki natury, takie jak świergot ptaków i szelest trawy.'), nl.
+
+describe(pustynia) :-
+    write('Wkraczasz na bezkresną pustynię, gdzie ziemia pokryta jest złotym piaskiem, a horyzont zdaje się nigdy nie kończyć.'), nl,
+    write('Słońce praży niebłaganie, tworząc fale gorącego powietrza, a cienie są niemal niemożliwe do znalezienia.'), nl,
+    write('W oddali widzisz majestatyczne wydmy, które zdają się tańczyć w migotliwym świetle pustynnego pejzażu.'), nl,
+    write('Pustynia jest jak krajobraz z innej planety, gdzie cisza jest tylko przerywana szumem wiatru i odgłosami kroków po piasku.'), nl.
+
+describe(mesa) :-
+    write('Stoisz na szczycie majestatycznej mesy, gdzie kamienne formacje tworzą surrealistyczny krajobraz.'), nl,
+    write('Słońce rzuca długie cienie na czerwoną ziemię, podkreślając kontury unikalnych kształtów skał.'), nl,
+    write('Mesa przypomina naturalne świątynie, a ich wysokie klify oferują zapierające dech w piersiach widoki na otaczający krajobraz.'), nl,
+    write('W powietrzu unosi się ciepły wiatr, przenoszący zapach suchej ziemi i roślinności adaptującej się do trudnych warunków.'), nl.
+
+describe(pustkowie1) :-
+    write('Wchodzisz na obszar pustkowia, gdzie ziemia jest spękana i sucha, a niebo zdaje się nie mieć końca.'), nl,
+    write('Nie ma tu żadnej roślinności, a jedynym towarzyszem jest skwarny wiatr unoszący kłęby kurzu w powietrzu.'), nl,
+    write('Na horyzoncie widzisz jedynie opustoszałe ruiny, pozostałość po dawnych cywilizacjach, które przepadły w zapomnieniu.'), nl,
+    write('Pustkowie emanuje atmosferą opuszczenia, a cisza jest jedynym dźwiękiem, jaki się tu rozchodzi.'), nl.
 
 % zasady przemiszczania sie
 map :-
