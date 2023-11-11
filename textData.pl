988bec886642eed78ceb9f06bc79b9d702b90822
jwozniak <01169260@pw.edu.pl>
2023-11-11 14:50:10 +0100

    dodano część opisów po przeszukaniu lokacji

--- textData.pl
+++ textData.pl
@@ -5,11 +5,23 @@ describe(pustkowie2) :-
     write('W oddali dostrzegasz samotne formacje skalne, które zdają się być ostatnimi świadkami dawnej obfitości tego terenu.'), nl,
     write('Pustkowie emanuje atmosferą opuszczenia, a cisza jest jedynym dźwiękiem, jaki się tu rozchodzi.'), nl.
 
+% Opis odkrytego kamienia +1 atak
+search(pustkowie2) :-
+    write('Rozglądasz się uważnie i dostrzegasz kamień, który może posłużyć za broń.'), nl,
+    write('Jest to solidny kamień o ostrym kształcie, nadający się do rzucania lub używania jako improwizowana broń.'), nl.
+
 describe(oceanStatek) :-
-    write('Stoisz na brzegu spokojnych wód oceanu, gdzie horyzont łączy się z niebem w jednym nieskończonym punkcie.'), nl,
-    write('W oddali dostrzegasz zatopiony wrak statku, jego zardzewiałe fragmenty unoszą się na powierzchni wody.'), nl,
-    write('Pęknięte burty i rozerwane żagle świadczą o dawnej chwale tego statku, który teraz spoczywa na dnie oceanu.'), nl,
-    write('Słyszysz odgłos fal oblewających wrak, a powietrze unosi się zapachem morskiej soli i historii opowiedzianej przez zatopiony statek.'), nl.
+    write('Unosisz nad dnem oceanu, gdzie rozciąga się zatopiony wrak statku, pozostawiający po sobie tylko fragmenty dawnej świetności.'), nl,
+    write('Roślinność morska oplata kadłub statku, a promienie słońca przebijające się przez wodę tworzą migotliwe światło na dnie oceanu.'), nl,
+    write('Wokół unoszą się ryby, a w szczelinach wraku odkrywasz tajemnice, które przetrwały upadek tego statku.'), nl,
+    write('Czujesz nutę tajemnicy, gdy zbliżasz się do zatopionego wraku, a woda rozchodzi się wokół ciebie jak cicha opowieść o przeszłości.'), nl.
+
+% Opis odkrytej części portalu 1/3, złotej bransoletki +2 życia
+search(oceanStatek) :-
+    write('Rozglądasz się wokół wraku i odkrywasz część portalu, która zdaje się być fragmentem starożytnego artefaktu.'), nl,
+    write('Część portalu jest wykonana z nieznanego metalu i emanuje słabym światłem, sugerując swoją magiczną naturę.'), nl,
+    write('Podczas przeszukiwania wnętrza wraku znajdujesz piękną złotą bransoletkę zdobioną misternymi wzorami.'), nl,
+    write('To wyjątkowy przedmiot, który zdaje się być starożytnym skarbem utraconym w czasie zatopienia statku.'), nl.
 
 describe(oceanPusty) :-
     write('Nurkujesz w głąb oceanu, a wokół ciebie rozciąga się pusty, bezkresny obszar wód głębinowych.'), nl,
@@ -17,23 +29,41 @@ describe(oceanPusty) :-
     write('Ciemność otacza cię ze wszystkich stron, a jedynym dźwiękiem jest łagodne szumienie wody przemieszczanej przez ruchy fal.'), nl,
     write('To miejsce emanuje spokojem, ale jednocześnie wzbudza uczucie nieskończoności i tajemnicy oceanicznej głębi.'), nl.
 
+search(oceanPusty) :-
+    write('Mimo starannej obserwacji, nie odkrywasz żadnych interesujących elementów tego obszaru oceanicznego.'), nl.
+
 describe(rafaKoralowa) :-
     write('Zanurzasz się w barwnym świecie rafy koralowej, gdzie tysiące koralowców tworzy niezwykłe formacje podwodne.'), nl,
     write('Rafa pulsuje życiem, a kolorowe ryby przeplatają się między koralami, tworząc hipnotyzujący taniec pod wodą.'), nl,
     write('Na dnie rafy dostrzegasz ukryte jaskinie i groty, które stanowią schronienie dla różnorodnych morskich istot.'), nl,
     write('Dźwięki wody i śpiewu morskich istot wypełniają przestrzeń, nadając temu miejscu magii i niepowtarzalnego piękna.'), nl.
 
+% Opis odkrytej skrzynki po podenisieniu zanjduejsz w niej zbroje hp +3
+search(rafaKoralowa) :-
+    write('Podczas nurkowania w rafie koralowej dostrzegasz starą skrzynkę, która leży między koralami.'), nl,
+    write('Skrzynka zdaje się być starożytna i pokryta kolorowymi morskimi roślinami, co czyni ją trudno dostrzegalną.'), nl.
+
+
 describe(jaskiniaPodwodna) :-
     write('Nurkujesz głęboko w czeluście podwodnej jaskini, gdzie światło słoneczne ledwo przenika przez kryształową wodę.'), nl,
     write('Ściany jaskini są pokryte koralowymi formacjami, a kolorowe ryby migoczą wśród zakamarków, tworząc podwodne arcydzieło natury.'), nl,
     write('Woda jest przejrzysta, a na dnie jaskini dostrzegasz ukryte jaskinie i szczeliny, które prowadzą do nieznanych zakątków podwodnego świata.'), nl,
+    write('W ciemnościach jaskini dostrzegasz podejrzany ruch, sugerujący obecność tajemniczych postaci ukrytych w cieniach.'), nl,
     write('Dźwięki wody i subtelne szmery morskich istot wypełniają przestrzeń, nadając temu miejscu magiczny i tajemniczy charakter.'), nl.
 
+search(jaskiniaPodwodna) :-
+    write('W jaskini podwodnej dostrzegasz jedynie pozostałości po dawnych ucztach utopców.'), nl.
+
 describe(obozRozbojnikow) :-
-    write('Znajdujesz się przed obozem rozbójników, ukrytym głęboko w gęstym lesie, z dala od cywilizacji.'), nl,
+    write('Znajdujesz się przed obozem rozbójników, ukrytym obok pustkowia w gęstym lesie, z dala od cywilizacji.'), nl,
     write('Na obrzeżach obozu widzisz prymitywne szałasy zrobione z gałęzi i skór, a dym z ognisk unosi się w powietrze.'), nl,
     write('Wokół obozu czujesz podejrzane spojrzenia, a z oddali dobiegają odgłosy dzikich zwierząt, które współistnieją z rozbójnikami.'), nl,
-    write('Strzeż się, gdyż to miejsce pełne jest tajemnic i niebezpieczeństw, które czyhają na każdego, kto zbliży się zbyt blisko.'), nl.
+    write('Gdy wkroczysz na teren obozu, nagle zostajesz zaatakowany przez grupę rozbójników! Musisz bronić się przed ich atakiem.'), nl.
+
+% Opis odkrytych skórzanych butów +2hp
+search(obozRozbojnikow) :-
+    write('Po pokonaniu rozbójników, przeszukujesz obozowe zaułki i znajdujesz skórzane buty, które mogą się okazać użyteczne w dalszych przygodach.'), nl,
+    write('Dodatkowo, natrafiasz na kilka pozostałości po wcześniejszych ofiarach rozbójników, ale większość wartościowych przedmiotów została rozgrabiona.'), nl.
 
 describe(rzeka) :-
     write('Stoisz na brzegu urokliwego jeziora, gdzie spokojna rzeka płynąca z gór wpada w jego nurt, tworząc malownicze połączenie wód.'), nl,
@@ -41,11 +71,21 @@ describe(rzeka) :-
     write('Ptaki wodne unoszą się na powierzchni jeziora, a nad brzegiem rosną kolorowe kwiaty, zdobiąc otoczenie tego magicznego zakątka.'), nl,
     write('Woda jeziora mieni się odcieniami niebieskiego, a promienie słońca odbijają się na powierzchni, jakby malowały błyszczący obraz natury.'), nl.
 
+search(rzeka) :-
+    write('Przeszukujesz brzegi rzeki, starannie zwracając uwagę na otoczenie.'), nl,
+    write('Niestety, nie dostrzegasz niczego szczególnie interesującego.'), nl,
+    write('Jedynym ruchem na horyzoncie są kilka ptaków wodnych, unoszących się na leniwym nurcie rzeki.'), nl,
+    write('Pomimo starannych poszukiwań, wydaje się, że rzeka nie kryje żadnych tajemniczych przedmiotów czy innych odkryć.'), nl.
+
 describe(kanion) :-
-    write('Stoisz na krawędzi wielkiego kanionu, gdzie ściany skalne wznoszą się ku niebu, tworząc imponujący labirynt głębokich wąwozów.'), nl,
-    write('W dnie kanionu płynie rwąca rzeka, której wody błyszczą w świetle słońca, tworząc błękitną nitkę wśród skalistego krajobrazu.'), nl,
-    write('Na ścianach skalnych dostrzegasz warstwy skał, które zdają się opowiadać historię wielowiekowych procesów geologicznych.'), nl,
-    write('W powietrzu unosi się świeży zapach wilgotnej ziemi, a echo odgłosów natury tworzy magię tego niezwykłego miejsca.'), nl.
+    write('Nagle zapada się pod tobą ziemia, cudem przeżywasz upadek jednak teraz znajdujesz sięna dnie wielkiego kanionu.'), nl,
+    write('Ściany skalne wznoszą się ku niebu, tworząc imponujący labirynt głębokich wąwozów.'), nl,
+    write('W dnie kanionu płynie mały strumyk, którego błyszczące wody stanowią błękitną nitkę wśród skalistego krajobrazu.'), nl.
+
+search(kanionu) :-    
+    write('Podczas przeszukiwania kanionu znajdujesz tylko kilka porozrzucanych kości,'), nl,
+    write('które wydają się być pozostałością po podróżnych, którzy nie mieli tyle szczęścia co ty.'), nl.
+
 
 describe(lasBrzozowy) :-
     write('Przechadzasz się przez uroczy las brzozowy, gdzie delikatne liście brzóz szumią w letnim wietrze.'), nl,
@@ -53,11 +93,28 @@ describe(lasBrzozowy) :-
     write('Ptaki śpiewają miłe melodie, tworząc harmonijną symfonię natury, a ścieżki wiodą cię między korzeniami i pniami brzóz.'), nl,
     write('W oddali słychać szmer strumienia, który dodaje uroku temu malowniczemu zakątkowi lasu.'), nl.
 
+% Opis akcji przeszukania lasu brzozowego, miód +2hp
+search(lasBrzozowy) :-
+    write('Przeszukując las brzozowy, twoje uszy wychwytują delikatny dźwięk brzęczenia.'), nl,
+    write('Podążasz za dźwiękiem i nagle trafiasz na ukryty ul pszczół, zbudowany w gęstym konarach drzew brzozowych.'), nl,
+    write('Pszczoły pracują gorliwie, zbierając nektar i wytwarzając pyszny miód, który przepełnia powietrze słodkim aromatem.'), nl,
+    write('To magiczne znalezisko w lesie brzozowym ukazuje nie tylko pracowitość pszczół, ale także możliwość zebrania smacznego miodu.'), nl.
+
 describe(jaskinia2) :-
     write('Wkraczasz do jaskini, a wnętrze mieni się blaskiem tysięcy kryształów o różnych kształtach i kolorach.'), nl,
     write('Ściany jaskini są dosłownie pokryte błyszczącymi klejnotami, które odbijają światło w fantastyczny sposób.'), nl,
     write('W powietrzu unoszą się iskry, tworząc magiczną aurę wokół ciebie, gdy kroczysz po posypanym kryształami podłożu.'), nl,
-    write('Delikatne dźwięki echo odbijają się od kryształów, tworząc hipnotyzującą symfonię, której nigdy wcześniej nie słyszałeś.'), nl.
+    write('Delikatne dźwięki echo odbijają się od kryształów, tworząc hipnotyzującą symfonię, której nigdy wcześniej nie słyszałeś.'), nl,
+    write('Nagle, z cienia jaskini wyłaniają się wrogie szkielety! Atakują cię zobacząc, ich oczy świecą złowrogo w mroku.'), nl,
+    write('Musisz bronić się przed tymi nieumarłymi przeciwnikami, by kontynuować swoją podróż przez tę magiczną, lecz niebezpieczną jaskinię.'), nl.
+
+% Żelazna zbroja +4hp i część portalu 2/3
+search(jaskinia2) :-
+    write('Po zakończonym starciu ze szkieletami przystępujesz do przeszukania jaskini w poszukiwaniu cennych przedmiotów.'), nl,
+    write('Okazuje się, że szkielety nie były tu bez powodu albowiem pilnowały one części portalu.'), nl,
+    write('Część portalu jest wykonana z nieznanego metalu i emanuje słabym światłem, sugerując swoją magiczną naturę.'), nl,
+    write('Twoje badania zostają nagrodzone, gdy natrafiasz na żelazną zbroję ukrytą w jednym z zakamarków.'), nl,
+    write('Zbroja jest solidna i wydaje się być w dobrym stanie, gotowa do założenia i ochrony przed kolejnymi niebezpieczeństwami.'), nl.
 
 describe(gory) :-
     write('Wspinasz się po majestatycznych szczytach gór, gdzie powietrze staje się rzadkie, a panorama roztacza się na wszystkie strony.'), nl,
